e6bcf25ac70f15a8a49e00848b8da778e54ef9b0
AgnikaSunil <agnika.sunil@gmail.com>
2021-01-25 16:36:24 +0530

    C84 Project

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,14 +1,53 @@
-import React,{Component} from 'react';
-import {Header, Icon, Badge} from 'react-native-elements';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import React, { Component} from 'react';
+import { Header,Icon,Badge } from 'react-native-elements';
+import { View, Text, StyeSheet ,Alert} from 'react-native';
+import db from '../config'
 
-const MyHeader = props =>{
-    return(
-        <Header
-            centerComponent = {{text: props.title, style: {color: '#90A5A9', fontSize: 20, fontWeight: "bold"}}}
-            backgroundColor = "#eaf8fe"
-        />
-    )
+export default class MyHeader extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      value:""
+    }
+  }
+
+getNumberOfUnreadNotifications(){
+  db.collection('all_notifications').where('notification_status','==',"unread")
+  .onSnapshot((snapshot)=>{
+    var unreadNotifications = snapshot.docs.map((doc) => doc.data())
+    this.setState({
+      value: unreadNotifications.length
+    })
+  })
 }
 
-export default MyHeader;
+componentDidMount(){
+  this.getNumberOfUnreadNotifications()
+}
+
+
+ BellIconWithBadge=()=>{
+    return(
+      <View>
+        <Icon name='bell' type='font-awesome' color='#696969' size={25}
+          onPress={() =>this.props.navigation.navigate('Notifications')}/>
+         <Badge
+          value={this.state.value}
+         containerStyle={{ position: 'absolute', top: -4, right: -4 }}/>
+      </View>
+    )
+  }
+
+  render(){
+    return(
+        <Header
+          leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => this.props.navigation.toggleDrawer()}/>}
+          centerComponent={{ text: this.props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
+          rightComponent={<this.BellIconWithBadge {...this.props}/>}
+          backgroundColor = "#eaf8fe"
+        />
+
+)
+}
+
+}
